c7969a0a440c842136e713d11e7cfab2e2178de0
[haezzang] <songna89@naver.com>
2022-04-14 14:45:25 +0900

    [JS] 함수의 출력

--- node.js/syntax/function2.js
+++ node.js/syntax/function2.js
@@ -2,10 +2,10 @@ console.log(Math.round(1.6));
 console.log(Math.round(1.4));
 
 function sum(first, second){
-    console.log(first+second);
+    return first+second;
 }
-sum(2,4);
 
+console.log(sum(2,4));
 // f123();
 // console.log('A');
 // console.log('Z');
